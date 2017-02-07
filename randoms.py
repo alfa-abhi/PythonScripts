0cc0a86d22b49af86b665d152618ec2a58f6f4b8
Abhinav Chauhan <alfa.abhi1996@gmail.com>
2017-02-07 20:35:52 +0530

    Update randoms.py

--- randoms.py
+++ randoms.py
@@ -7,21 +7,20 @@ exR = csv.reader(exF)
 exD = list(exR)
 print exD
 
-db = MySQLdb.connect("localhost", "root", "2801", "addressanalysis")
+db = MySQLdb.connect("host", "username", "password", "schema_name")
 cursor = db.cursor()
 
 k = 0
-for i in exD:
+for i in exD: //My Database had 6 fields and first were 5 filled by csv columns (record held by variable 'i'), change accordingly.
     if k == 0:
         pass
-    else:
-        if i[4] == "Delhi" or i[4] == "DELHI":
-            query = 'INSERT INTO addressanalysis.addresses VALUES("' + i[0] + '","' + i[1] + '",' + i[2] + ',"' + i[3] + '","' + i[4] + '",' + str(k) + ');'
-            try:
-                cursor.execute(query)
-                db.commit()
-            except:
-                print k
+    else: //table already exists, else execute a create table query first.
+        query = 'INSERT INTO schema_name.table_name VALUES("' + i[0] + '","' + i[1] + '",' + i[2] + ',"' + i[3] + '","' + i[4] + '",' + str(k) + ');'
+        try:
+            cursor.execute(query)
+            db.commit()
+        except:
+            print k
     k += 1
 
 db.close()
